32fdbc6f91397e8d932904c5ff87d8955b9c35c9
Marie-Paule Bouvy <nicola.iezzi@gmail.com>
2025-10-08 14:28:31 +0200

    Mise à jour du projet radar

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,10 @@ ROOT_URLCONF = "config.urls"
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 STATIC_URL = "/static/"
